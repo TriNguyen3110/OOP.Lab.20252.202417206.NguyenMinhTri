592fe31d1434ef078bcaeac8a3ae6eb040fdb092
TriNguyen3110 <tringuyen311006@gmail.com>
2026-04-13 14:34:11 +0700

    Other projects

---
+++ OtherProjects/Src/hust/soict/globalict/Lab01/ChoosingOption.java
@@ -0,0 +1,10 @@
+package hust.soict.globalict.Lab01;
+
+import javax.swing.JOptionPane;
+
+public class ChoosingOption {
+    public static void main(String[] args) {
+        int option = JOptionPane.showConfirmDialog(null, "Do you want to change the first class ticket?");
+        JOptionPane.showMessageDialog(null, "You've chosen: " + (option == JOptionPane.YES_OPTION?"Yes":"No"));
+    }
+}
