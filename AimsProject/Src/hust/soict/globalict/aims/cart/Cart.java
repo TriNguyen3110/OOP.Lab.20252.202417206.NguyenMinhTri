95e4c2f0c3fafa91eb8b064bbb16953bd60ac3a0
TriNguyen3110 <tringuyen311006@gmail.com>
2026-04-15 23:07:52 +0700

    Print the list of ordered items of a cart, the price of each item, and the total price

--- AimsProject/Src/hust/soict/globalict/aims/cart/Cart.java
+++ AimsProject/Src/hust/soict/globalict/aims/cart/Cart.java
@@ -69,15 +69,22 @@ public class Cart {
         return s;
     }
 
-    public void displayCart(){
-        System.out.println("----- MY CART -----");
+    public void displayCart() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
         for (int i = 0; i < qtyOrdered; i++) {
-            System.out.printf("%-3d\t%-15s\t%7.2f\n", 
-                i + 1, 
-                myCart[i].getTitle(), 
-                myCart[i].getCost()
+            DigitalVideoDisc dvd = myCart[i];
+            // DVD - [Title] - [category] - [Director] - [Length]: [Price]$
+            System.out.printf("%d. DVD - %s - %s - %s - %d: %.2f $\n",
+                    dvd.getID(),
+                    dvd.getTitle(),
+                    dvd.getCategory(),
+                    dvd.getDirector(),
+                    dvd.getLength(),
+                    dvd.getCost()
             );
         }
-        System.out.printf("Total Cost\t%.2f\n", totalCost());
+        System.out.printf("Total cost: %.2f $\n", totalCost());
+        System.out.println("***************************************************");
     }
 }
