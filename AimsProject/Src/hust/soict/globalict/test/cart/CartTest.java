58dd3eef8141a9dc46c32414951b04021f07fbf8
TriNguyen3110 <tringuyen311006@gmail.com>
2026-04-15 23:43:10 +0700

    Update CartTest

--- AimsProject/Src/hust/soict/globalict/test/cart/CartTest.java
+++ AimsProject/Src/hust/soict/globalict/test/cart/CartTest.java
@@ -6,12 +6,23 @@ import hust.soict.globalict.aims.disc.DigitalVideoDisc;
 public class CartTest {
     public static void main(String[] args) {
         Cart cart = new Cart();
+
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Sci-Fi", "George Lucas", 124, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "Guy Ritchie", 90, 18.99f);
+
         cart.addDigitalVideoDisc(dvd1);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
         cart.addDigitalVideoDisc(dvd2);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
         cart.addDigitalVideoDisc(dvd3);
+
         cart.displayCart();
+
+        System.out.println("\n--- Testing Search by ID ---");
+        cart.searchById(1);
+        cart.searchById(5);
+
+        System.out.println("\n--- Testing Search by Title ---");
+        cart.searchByTitle("Star Wars");
+        cart.searchByTitle("Avengers");
     }
 }
