fbfede372237ac25f9bc7e714478619581d4f9f3
TriNguyen3110 <tringuyen311006@gmail.com>
2026-04-15 23:26:42 +0700

    CartTest class

--- AimsProject/Src/hust/soict/globalict/test/cart/CartTest.java
+++ AimsProject/Src/hust/soict/globalict/test/cart/CartTest.java
@@ -10,8 +10,7 @@ public class CartTest {
         cart.addDigitalVideoDisc(dvd1);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
         cart.addDigitalVideoDisc(dvd2);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f
-        );
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
         cart.addDigitalVideoDisc(dvd3);
         cart.displayCart();
     }
