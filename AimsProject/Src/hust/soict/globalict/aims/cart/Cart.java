69fb38217e27a40f653b1c2d6e6ea1e6abc432a9
TriNguyen3110 <tringuyen311006@gmail.com>
2026-04-16 22:27:26 +0700

    Update Cart.java

--- AimsProject/Src/hust/soict/globalict/aims/cart/Cart.java
+++ AimsProject/Src/hust/soict/globalict/aims/cart/Cart.java
@@ -97,7 +97,7 @@ public class Cart {
         return s;
     }
 
-    public void displayCart() {
+    public void print() {
         System.out.println("***********************CART***********************");
         System.out.println("Ordered Items:");
         for (int i = 0; i < qtyOrdered; i++) {
