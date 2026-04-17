8b95e45dbcd5b8b0b750678dde8a2429a0f1e0b3
TriNguyen3110 <tringuyen311006@gmail.com>
2026-04-17 15:32:53 +0700

    NoGarbage program

--- OtherProjects/Src/hust/soict/globalict/garbage/NoGarbage.java
+++ OtherProjects/Src/hust/soict/globalict/garbage/NoGarbage.java
@@ -1,4 +1,29 @@
 package hust.soict.globalict.garbage;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class NoGarbage {
+    public static void main(String[] args) {
+        String filename = "Lab03.pdf";
+        byte[] inputBytes = {0};
+        long startTime, endTime;
+
+        try{
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        startTime = System.currentTimeMillis();
+        StringBuffer outputStringBuffer = new StringBuffer();
+        for (byte b : inputBytes){
+            outputStringBuffer.append((char) b);
+        }
+        String outputString = outputStringBuffer.toString();
+        endTime = System.currentTimeMillis();
+        System.out.println("Processing time with StringBuffer: " + (endTime - startTime) + " ms");
+    }
 }
