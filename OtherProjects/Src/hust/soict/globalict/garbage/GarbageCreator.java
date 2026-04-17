c186e5c17f6c3acc795f89e59e8b211cdcf0e35f
TriNguyen3110 <tringuyen311006@gmail.com>
2026-04-17 15:32:19 +0700

    GarbageCreator program

--- OtherProjects/Src/hust/soict/globalict/garbage/GarbageCreator.java
+++ OtherProjects/Src/hust/soict/globalict/garbage/GarbageCreator.java
@@ -1,4 +1,28 @@
 package hust.soict.globalict.garbage;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class GarbageCreator {
+    public static void main(String[] args) {
+        String filename = "Lab03.pdf";
+        byte[] inputBytes = {0};
+        long startTime, endTime;
+
+        try{
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        startTime = System.currentTimeMillis();
+        String outputString = "";
+        for (byte b : inputBytes){
+            outputString += (char) b;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Processing time with + operator: " + (endTime - startTime) + " ms");
+    }
 }
