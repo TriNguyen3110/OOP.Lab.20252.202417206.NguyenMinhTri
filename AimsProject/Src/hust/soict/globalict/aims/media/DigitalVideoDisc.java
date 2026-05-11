4dfb7dee3b197fc0b3616b85b5052423919d370e
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-11 14:47:58 +0700

    Modify DigitalVideoDisc class

--- AimsProject/Src/hust/soict/globalict/aims/media/DigitalVideoDisc.java
+++ AimsProject/Src/hust/soict/globalict/aims/media/DigitalVideoDisc.java
@@ -1,78 +1,67 @@
-package hust.soict.globalict.aims.disc;
+package hust.soict.globalict.aims.media;
 
-public class DigitalVideoDisc {
-        private int id;
-        private String title;
-        private String category;
-        private String director;
-        private int length;
-        private float cost;
+public class DigitalVideoDisc extends Media {
+    private String director;
+    private int length;
 
-        private static int nbDigitalVideoDiscs = 0;
+    private static int nbDigitalVideoDiscs = 0;
 
-        public DigitalVideoDisc(String title){
-            this.title = title;
-            updateID();
-        }
+    public DigitalVideoDisc(String title){
+        super();
+        this.setTitle(title);
+        updateID();
+    }
 
-        public DigitalVideoDisc(String category, String title, float cost){
-            this.title = title;
-            this.category = category;
-            this.cost = cost;
-            updateID();
-        }
+    public DigitalVideoDisc(String category, String title, float cost){
+        super();
+        this.setCategory(category);
+        this.setTitle(title);
+        this.setCost(cost);
+        updateID();
+    }
 
-        public DigitalVideoDisc(String director, String category, String title, float cost){
-            this.director = director;
-            this.category = category;
-            this.title = title;
-            this.cost = cost;
-            updateID();
-        }
+    public DigitalVideoDisc(String director, String category, String title, float cost){
+        super();
+        this.director = director;
+        this.setCategory(category);
+        this.setTitle(title);
+        this.setCost(cost);
+        updateID();
+    }
 
-        public DigitalVideoDisc(String title, String category, String director, int length, float cost){
-            this.title = title;
-            this.category = category;
-            this.director = director;
-            this.length = length;
-            this.cost = cost;
-            updateID();
-        }
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost){
+        super();
+        this.setTitle(title);
+        this.setCategory(category);
+        this.director = director;
+        this.length = length;
+        this.setCost(cost);
+        updateID();
+    }
 
-        private void updateID(){
-            nbDigitalVideoDiscs ++;
-            this.id = nbDigitalVideoDiscs;
-        }
+    private void updateID(){
+        nbDigitalVideoDiscs ++;
+        this.setId(nbDigitalVideoDiscs);
+    }
 
-        public boolean isMatch(String title){
-            return this.title.equalsIgnoreCase(title);
-        }
+    public boolean isMatch(String title){
+        return this.getTitle().equalsIgnoreCase(title);
+    }
 
-        @Override
-        public String toString(){
-            return  id + ".DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + " $";
-        }
+    @Override
+    public String toString(){
+        return  getId() + ".DVD - " + getTitle() + " - " + getCategory() + " - " + director + " - " + length + ": " + getCost() + " $";
+    }
 
-        public int getID(){
-            return id;
-        }
-        public String getTitle(){
-            return title;
-        }
-        public String getCategory(){
-            return category;
-        }
-        public String getDirector(){
-            return director;
-        }
-        public int getLength(){
-            return length;
-        }
-        public float getCost(){
-            return cost;
-        }
+    public String getDirector(){
+        return director;
+    }
+    public int getLength(){
+        return length;
+    }
 
-        public void setTitle(String title){
-            this.title = title;
-        }
+    @Override
+    public void setTitle(String title){
+        super.setTitle(title);
+    }
 }
