860752c2f2ba3908746c08b1e7ad0ee36e57c6af
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-11 14:46:08 +0700

    Modify Book class

--- AimsProject/Src/hust/soict/globalict/aims/media/Book.java
+++ AimsProject/Src/hust/soict/globalict/aims/media/Book.java
@@ -3,38 +3,11 @@ package hust.soict.globalict.aims.media;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Book {
-    private int id;
-    private String title;
-    private String category;
-    private float cost;
+public class Book extends Media {
     private List<String> authors = new ArrayList<String>();
 
     public Book(int id, String title, float cost, String category, List<String> authors) {
-        this.id = id;
-        this.title = title;
-        this.cost = cost;
-        this.category = category;
-        this.authors = authors;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public void setCost(float cost) {
-        this.cost = cost;
-    }
-
-    public void setAuthors(List<String> authors) {
+        super(id, title, category, cost);
         this.authors = authors;
     }
 
