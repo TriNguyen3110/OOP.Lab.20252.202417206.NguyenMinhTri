1c78f9fcbb46367e21eda518364c08088ab51023
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-11 14:47:17 +0700

    Create the abstract Media class

--- AimsProject/Src/hust/soict/globalict/aims/media/Media.java
+++ AimsProject/Src/hust/soict/globalict/aims/media/Media.java
@@ -1,4 +1,51 @@
 package hust.soict.globalict.aims.media;
 
-public class Media {
+public abstract class Media {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+
+    public Media(int id, String title, String category, float cost) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public Media() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
 }
