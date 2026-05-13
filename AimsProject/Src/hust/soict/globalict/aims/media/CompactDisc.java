d0fca6577314132bed5aee81cc0a83847f7ccc12
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-13 22:11:20 +0700

    Implement play() for Compact Disk

--- AimsProject/Src/hust/soict/globalict/aims/media/CompactDisc.java
+++ AimsProject/Src/hust/soict/globalict/aims/media/CompactDisc.java
@@ -3,7 +3,7 @@ package hust.soict.globalict.aims.media;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CompactDisc extends Disc {
+public class CompactDisc extends Disc implements Playable {
     private String artist;
     private List<Track> tracks = new ArrayList<>();
 
@@ -43,5 +43,14 @@ public class CompactDisc extends Disc {
             totalLength += track.getLength();
         }
         return totalLength;
+    }
+
+    @Override
+    public void play(){
+        System.out.println("Playing CD: " + this.getTitle() + " by " + this.getArtist());
+        System.out.println("CD length: " + this.getLength());
+        for(Track track : tracks){
+            track.play();
+        }
     }
 }
