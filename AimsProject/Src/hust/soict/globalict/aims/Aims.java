3187ee12c02f65d0f2d95a76140b5a4860722baf
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-13 23:26:27 +0700

    Update Aims class

--- AimsProject/Src/hust/soict/globalict/aims/Aims.java
+++ AimsProject/Src/hust/soict/globalict/aims/Aims.java
@@ -1,45 +1,58 @@
 package hust.soict.globalict.aims;
 
 import hust.soict.globalict.aims.cart.Cart;
-import hust.soict.globalict.aims.disc.DigitalVideoDisc;
+import hust.soict.globalict.aims.media.DigitalVideoDisc;
+import hust.soict.globalict.aims.media.Media;
 
 public class Aims {
     public static void main(String[] args) {
         Cart myCart = new Cart();
+
+        // Tạo các đối tượng DVD
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Sci-Fi", "George Lucas", 124, 24.95f);
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "Guy Ritchie", 90, 18.99f);
         DigitalVideoDisc dvd4 = new DigitalVideoDisc("Avengers", "Action", "Russo Brothers", 143, 29.99f);
         DigitalVideoDisc dvd5 = new DigitalVideoDisc("Batman", "Action", "Christopher Nolan", 140, 22.50f);
 
-        // Add DVD
+        // 1. Thêm lẻ từng DVD (Sử dụng addMedia thay cho addDigitalVideoDisc)
         System.out.println("-----ADDING-----");
-        myCart.addDigitalVideoDisc(dvd1);
-        myCart.addDigitalVideoDisc(dvd2);
-        myCart.addDigitalVideoDisc(dvd3);
+        myCart.addMedia(dvd1);
+        myCart.addMedia(dvd2);
+        myCart.addMedia(dvd3);
         myCart.print();
 
-        // Remove DVD
+        // 2. Xóa DVD (Sử dụng removeMedia)
         System.out.println("-----REMOVING-----");
-        myCart.removeDigitalVideoDisc(dvd2);
+        myCart.removeMedia(dvd2);
         myCart.print();
 
-        // Remove unknown DVD
-        System.out.println("-----TEST REMOVE UNKOWN-----");
+        // 3. Xóa DVD không tồn tại
+        System.out.println("-----TEST REMOVE UNKNOWN-----");
         DigitalVideoDisc fakeDVD = new DigitalVideoDisc("Fake", "Test", "Unknown", 100, 10f);
-        myCart.removeDigitalVideoDisc(fakeDVD);
+        myCart.removeMedia(fakeDVD);
         myCart.print();
 
-        System.out.println("\n-----TEST ADD LIST (OVERLOADING)-----");
-        DigitalVideoDisc[] dvdList = {dvd4, dvd5};
-        myCart.addDigitalVideoDisc(dvdList);
+        // 4. Thêm danh sách DVD (Sử dụng vòng lặp vì addMedia nhận từng Media)
+        System.out.println("\n-----TEST ADD LIST-----");
+        Media[] dvdList = {dvd4, dvd5};
+        for (Media m : dvdList) {
+            myCart.addMedia(m);
+        }
         myCart.print();
 
-        System.out.println("\n-----TEST ADD TWO DVDs (OVERLOADING)-----");
+        // 5. Thêm hai DVD
+        System.out.println("\n-----TEST ADD TWO DVDs-----");
         DigitalVideoDisc dvd6 = new DigitalVideoDisc("Spider-Man", "Action", "Jon Watts", 133, 21.00f);
         DigitalVideoDisc dvd7 = new DigitalVideoDisc("Cinderella", "Animation", "Kenneth Branagh", 105, 15.50f);
 
-        myCart.addDigitalVideoDisc(dvd6, dvd7);
+        myCart.addMedia(dvd6);
+        myCart.addMedia(dvd7);
         myCart.print();
+
+        // 6. Test tìm kiếm (Giữ nguyên tên phương thức bạn đã code trong Cart)
+        System.out.println("\n-----TEST SEARCH-----");
+        myCart.searchByTitle("Aladdin");
+        myCart.searchById(1);
     }
 }
