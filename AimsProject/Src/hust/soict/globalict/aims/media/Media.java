99e2c24d5f3b074e61e4ace60e0700024cb69ddb
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-13 22:47:39 +0700

    Modify Media class for implementing search methods

--- AimsProject/Src/hust/soict/globalict/aims/media/Media.java
+++ AimsProject/Src/hust/soict/globalict/aims/media/Media.java
@@ -48,4 +48,8 @@ public abstract class Media {
     public void setCost(float cost) {
         this.cost = cost;
     }
+
+    public boolean isMatch(String title){
+        return this.getTitle().equalsIgnoreCase(title);
+    }
 }
