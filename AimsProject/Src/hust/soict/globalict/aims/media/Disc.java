4166ffb1324bd228044fe0e340ca6c710c6f2f20
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-13 17:12:40 +0700

    Create the Disc class extending the Media class

---
+++ AimsProject/Src/hust/soict/globalict/aims/media/Disc.java
@@ -0,0 +1,35 @@
+package hust.soict.globalict.aims.media;
+
+public class Disc extends Media{
+    private int length;
+    private String director;
+
+    public Disc(){
+        super();
+    }
+
+    public Disc(String title, String category, String director, int length, float cost) {
+        super();
+        this.setTitle(title);
+        this.setCategory(category);
+        this.director = director;
+        this.length = length;
+        this.setCost(cost);
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+}
