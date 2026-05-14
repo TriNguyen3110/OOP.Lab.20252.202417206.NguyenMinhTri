8f76e19d25fdd161f48b676c68c35340283ad85e
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-14 09:47:09 +0700

    Update DigitalVideoDisc

--- AimsProject/Src/hust/soict/globalict/aims/media/DigitalVideoDisc.java
+++ AimsProject/Src/hust/soict/globalict/aims/media/DigitalVideoDisc.java
@@ -3,29 +3,6 @@ package hust.soict.globalict.aims.media;
 public class DigitalVideoDisc extends Disc implements Playable {
     private static int nbDigitalVideoDiscs = 0;
 
-    public DigitalVideoDisc(String title){
-        super();
-        this.setTitle(title);
-        updateID();
-    }
-
-    public DigitalVideoDisc(String category, String title, float cost){
-        super();
-        this.setCategory(category);
-        this.setTitle(title);
-        this.setCost(cost);
-        updateID();
-    }
-
-    public DigitalVideoDisc(String director, String category, String title, float cost){
-        super();
-        this.setDirector(director);
-        this.setCategory(category);
-        this.setTitle(title);
-        this.setCost(cost);
-        updateID();
-    }
-
     public DigitalVideoDisc(String title, String category, String director, int length, float cost){
         super(title, category, director, length, cost);
         updateID();
