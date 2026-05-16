f958b9e9da00ef6ca43cd81ae48daf035ab550eb
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-16 18:01:32 +0700

    Update StoreTest

--- AimsProject/Src/hust/soict/globalict/test/store/StoreTest.java
+++ AimsProject/Src/hust/soict/globalict/test/store/StoreTest.java
@@ -9,10 +9,10 @@ public class StoreTest {
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 90, 18.99f);
-        store.addDVD(dvd1);
-        store.addDVD(dvd2);
-        store.addDVD(dvd3);
-        store.removeDVD(dvd2);
-        store.removeDVD(new DigitalVideoDisc("Cinderella", "Animation", "Disney", 75, 15.99f));
+        store.addMedia(dvd1);
+        store.addMedia(dvd2);
+        store.addMedia(dvd3);
+        store.removeMedia(dvd2);
+        store.removeMedia(new DigitalVideoDisc("Cinderella", "Animation", "Disney", 75, 15.99f));
     }
 }
