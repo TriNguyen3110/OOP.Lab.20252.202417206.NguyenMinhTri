8da28c0c2b9fa2f18f013bd601d1d6e782be5f9c
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-16 17:58:02 +0700

    Update Disk

--- AimsProject/Src/hust/soict/globalict/aims/media/Disc.java
+++ AimsProject/Src/hust/soict/globalict/aims/media/Disc.java
@@ -24,12 +24,4 @@ public class Disc extends Media{
     public String getDirector() {
         return director;
     }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
 }
