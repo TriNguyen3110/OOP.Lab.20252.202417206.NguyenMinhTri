3ccc6cff62cfa59fbe725544a3ad675eb4bed78f
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-16 08:46:36 +0700

    Update Aims for testing polymorphism

--- AimsProject/Src/hust/soict/globalict/aims/Aims.java
+++ AimsProject/Src/hust/soict/globalict/aims/Aims.java
@@ -1,8 +1,13 @@
 package hust.soict.globalict.aims;
 
 import hust.soict.globalict.aims.cart.Cart;
+import hust.soict.globalict.aims.media.Book;
+import hust.soict.globalict.aims.media.CompactDisc;
 import hust.soict.globalict.aims.media.DigitalVideoDisc;
-import hust.soict.globalict.aims.media.Media;
+import hust.soict.globalict.aims.media.Track;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Aims {
     public static void main(String[] args) {
@@ -10,42 +15,27 @@ public class Aims {
 
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Sci-Fi", "George Lucas", 124, 24.95f);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "Guy Ritchie", 90, 18.99f);
-        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Avengers", "Action", "Russo Brothers", 143, 29.99f);
-        DigitalVideoDisc dvd5 = new DigitalVideoDisc("Batman", "Action", "Christopher Nolan", 140, 22.50f);
 
-        System.out.println("-----ADDING-----");
+        Book book1 = new Book(10, "Java Programming", 29.99f, "Education", new ArrayList<>(Arrays.asList("James Gosling", "Joshua Bloch")));
+        Book book2 = new Book(11, "Design Patterns", 39.50f, "Technology", new ArrayList<>(Arrays.asList("Erich Gamma")));
+
+        CompactDisc cd1 = new CompactDisc("Beethoven Symphony No. 9", "Classical", "Herbert von Karajan", 74, 15.00f, "Ludwig van Beethoven");
+
+        Track track1 = new Track("Allegro ma non troppo", 18);
+        Track track2 = new Track("Molto vivace", 12);
+        Track track3 = new Track("Adagio molto e cantabile", 16);
+
+        cd1.addTrack(track1);
+        cd1.addTrack(track2);
+        cd1.addTrack(track3);
+
+        System.out.println("----- ADDING MULTIPLE TYPES OF MEDIA -----");
         myCart.addMedia(dvd1);
+        myCart.addMedia(book1);
+        myCart.addMedia(cd1);
         myCart.addMedia(dvd2);
-        myCart.addMedia(dvd3);
+        myCart.addMedia(book2);
+
         myCart.print();
-
-        System.out.println("-----REMOVING-----");
-        myCart.removeMedia(dvd2);
-        myCart.print();
-
-        System.out.println("-----TEST REMOVE UNKNOWN-----");
-        DigitalVideoDisc fakeDVD = new DigitalVideoDisc("Fake", "Test", "Unknown", 100, 10f);
-        myCart.removeMedia(fakeDVD);
-        myCart.print();
-
-        System.out.println("\n-----TEST ADD LIST-----");
-        Media[] dvdList = {dvd4, dvd5};
-        for (Media m : dvdList) {
-            myCart.addMedia(m);
-        }
-        myCart.print();
-
-        System.out.println("\n-----TEST ADD TWO DVDs-----");
-        DigitalVideoDisc dvd6 = new DigitalVideoDisc("Spider-Man", "Action", "Jon Watts", 133, 21.00f);
-        DigitalVideoDisc dvd7 = new DigitalVideoDisc("Cinderella", "Animation", "Kenneth Branagh", 105, 15.50f);
-
-        myCart.addMedia(dvd6);
-        myCart.addMedia(dvd7);
-        myCart.print();
-
-        System.out.println("\n-----TEST SEARCH-----");
-        myCart.searchByTitle("Aladdin");
-        myCart.searchById(1);
     }
 }
