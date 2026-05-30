ae0cc9717719716b05b806903a5c8c7d8af8bed5
TriNguyen3110 <tringuyen311006@gmail.com>
2026-05-30 15:11:11 +0700

    Updating buttons based on selected item in TableView – ChangeListener

--- AimsProject/Src/hust/soict/globalict/aims/screen/CartScreenController.java
+++ AimsProject/Src/hust/soict/globalict/aims/screen/CartScreenController.java
@@ -3,19 +3,22 @@ package hust.soict.globalict.aims.screen;
 import hust.soict.globalict.aims.cart.Cart;
 import hust.soict.globalict.aims.media.Media;
 
+import hust.soict.globalict.aims.media.Playable;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import javafx.beans.value.ChangeListener;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CartScreenController implements Initializable {
-
     private Cart cart;
 
     @FXML
@@ -30,24 +33,45 @@ public class CartScreenController implements Initializable {
     @FXML
     private TableColumn<Media, Float> colMediaCost;
 
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnRemove;
+
     public CartScreenController(Cart cart) {
         this.cart = cart;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        colMediaTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colMediaCategory.setCellValueFactory(new PropertyValueFactory<>("category"));
+        colMediaCost.setCellValueFactory(new PropertyValueFactory<>("cost"));
 
-        // map dữ liệu cho các cột
-        colMediaTitle.setCellValueFactory(
-                new PropertyValueFactory<>("title"));
-
-        colMediaCategory.setCellValueFactory(
-                new PropertyValueFactory<>("category"));
-
-        colMediaCost.setCellValueFactory(
-                new PropertyValueFactory<>("cost"));
-
-        // đổ dữ liệu vào bảng
         tblMedia.setItems(cart.getItemsOrdered());
+
+        btnPlay.setVisible(false);
+        btnRemove.setVisible(false);
+
+        tblMedia.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Media>() {
+            @Override
+            public void changed(ObservableValue<? extends Media> observable, Media oldValue, Media newValue) {
+                if (newValue != null) {
+                    updateButtonBar(newValue);
+                }
+            }
+        });
+    }
+
+    void updateButtonBar(Media media) {
+        btnRemove.setVisible(true);
+
+        if (media instanceof Playable) {
+            btnPlay.setVisible(true);
+        }
+        else {
+            btnPlay.setVisible(false);
+        }
     }
 }
